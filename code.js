8296c0952e99ca437daf3a8977bb9d808139c102
EdiChenCool <108492465+EdiChenCool@users.noreply.github.com>
2024-05-30 11:44:11 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -7,7 +7,6 @@ function toggleDropdown() {
     }
 }
 
-// Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.dropdown-btn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
